543daf8e8d12f6636ee0c105d393b7ad3e1117e1
Sangwook Lee <leesang627@gmail.com>
2025-10-01 14:15:05 -0400

    change name of repository

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "CG Interface",
+  title: "Rylai",
   description: "Educational intervention for online unwanted sexual solicitations",
 };
 
